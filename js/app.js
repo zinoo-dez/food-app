a7097994807ed997a157179054d2df894e2cdf97
zinoo-jsx <zinoo.dev@gmail.com>
2023-09-13 16:53:32 +0630

    javascript file fiex(navbar error)

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ bars.addEventListener("click", function () {
   navLinks.classList.toggle("show");
   bars.classList.toggle("fa-times");
 });
+window.addEventListener("scroll", function () {
+  navLinks.classList.remove("show");
+  bars.classList.remove("fa-times");
+});
